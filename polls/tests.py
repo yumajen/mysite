9ae46b1d83764c9c3faaf9c7659481774f04173c
yumajen <okabe.yuma@jenesty.co.jp>
2019-05-22 22:52:23 +0900

    DetailViewに投稿日制約を追加

--- polls/tests.py
+++ polls/tests.py
@@ -53,6 +53,19 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 2.>',
                                                                             '<Question: Past question 1.>'])
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
 
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
